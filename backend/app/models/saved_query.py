5866e6885a7fc2c7640af443435e39f0f89e4618
felipe-ra <felipeangel50@gmail.com>
2023-11-20 22:39:08 -0500

    improved models documentation

--- backend/app/models/saved_query.py
+++ backend/app/models/saved_query.py
@@ -1,10 +1,18 @@
 # app/models/saved_query.py
 
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, Index, Sequence
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, Index
 from .base import Base
 
+
+
 class SavedQuery(Base):
-    __tablename__ = 'saved_queries'
+
+    """
+    Defines the SQLAlchemy ORM model for the 'saved_queries' table.
+    """
+
+    
+    __tablename__ = 'saved_queries'  # Name of the table in the database.
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
@@ -13,6 +21,7 @@ class SavedQuery(Base):
     comment = Column(Text)
     created_at = Column(DateTime)
 
+    # Define an index on the username column to improve query performance.
     __table_args__ = (
         Index('idx_saved_queries_username', 'username'),
     )
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,8 +3,14 @@
 from sqlalchemy import Column, String, DateTime
 from .base import Base 
 
-class User(Base):
-    __tablename__ = 'users'
 
-    username = Column(String, primary_key=True)
-    registered_at = Column(DateTime)
+class User(Base):
+
+    """
+    Defines the SQLAlchemy ORM model for the 'users' table.
+    """
+
+    __tablename__ = 'users'  # Name of the table in the database.
+
+    username = Column(String, primary_key=True)  # Unique identifier for each user.
+    registered_at = Column(DateTime)  # Timestamp of when the user first registered.
