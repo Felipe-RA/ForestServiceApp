a29d01c50f3fd443850424397622519ad554dc86
felipe-ra <felipeangel50@gmail.com>
2023-11-21 13:31:05 -0500

    added explicit collec coverage array for coverage

--- frontend/next-pages/jest.config.js
+++ frontend/next-pages/jest.config.js
@@ -5,6 +5,11 @@ module.exports = {
       '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     },
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    collectCoverageFrom: [ // Define the files Jest should collect coverage from
+    "app/**/*.tsx", // get source files from app
+    "!app/**/*.d.ts", // exclude TypeScript declaration files
+    // add more patterns as needed
+    ],
     coveragePathIgnorePatterns: [
       "/node_modules/",
       "<rootDir>/__tests__/",
