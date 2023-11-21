23ae9389e471b6ab68c370cb61bbf7da3d0daa72
felipe-ra <felipeangel50@gmail.com>
2023-11-21 13:17:27 -0500

    fixed jest dom config error

--- frontend/next-pages/jest.config.js
+++ frontend/next-pages/jest.config.js
@@ -4,11 +4,13 @@ module.exports = {
     transform: {
       '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     coveragePathIgnorePatterns: [
-        "/node_modules/",
-        "<rootDir>/__tests__/",
-        "<rootDir>/pages/_app.js",
-        "<rootDir>/pages/_document.js",
-        // we will put here more ignore patterns here, if needed
-      ],
+      "/node_modules/",
+      "<rootDir>/__tests__/",
+      "<rootDir>/pages/_app.js",
+      "<rootDir>/pages/_document.js",
+      // Add other paths to ignore here if we need them
+    ],
   };
+  
