c38f8c15e38d7fab608f7fa2e844c897e8bb743c
felipe-ra <felipeangel50@gmail.com>
2023-11-21 14:11:15 -0500

    fixed double configuration error

--- frontend/next-pages/jest.config.js
+++ frontend/next-pages/jest.config.js
@@ -1,21 +1,22 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    collectCoverageFrom: [ // Define the files Jest should collect coverage from
-    "app/**/*.tsx", // get source files from app
-    "!app/**/*.d.ts", // exclude TypeScript declaration files
-    // add more patterns as needed
-    ],
-    coveragePathIgnorePatterns: [
-      "/node_modules/",
-      "<rootDir>/__tests__/",
-      "<rootDir>/pages/_app.js",
-      "<rootDir>/pages/_document.js",
-      // Add other paths to ignore here if we need them
-    ],
-  };
-  
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverageFrom: [
+    "app/**/*.tsx", // Collect coverage from .tsx files under the app folder
+    "!app/**/*.d.ts", // Exclude TypeScript declaration files
+    // Add other patterns to exclude files as needed
+  ],
+  coverageDirectory: "<rootDir>/coverage", // The directory where Jest should output coverage files
+  coverageReporters: ["json", "lcov", "text", "clover"], // Reporters you want Jest to use
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/__tests__/", // Exclude test files themselves from coverage
+    "<rootDir>/pages/_app.js",
+    "<rootDir>/pages/_document.js",
+    // Add other paths to ignore here if needed
+  ],
+};
