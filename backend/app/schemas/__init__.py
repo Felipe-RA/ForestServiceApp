555a8e7f8c3ac7aa32b662781e22d76e10806cbb
felipe-ra <felipeangel50@gmail.com>
2023-11-21 16:09:12 -0500

    made schemas available from schema package

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from .user import UserBase
+from .user import UserCreate
+from .user import User
+
+from .saved_query import SavedQueryBase
+from .saved_query import SavedQueryCreate
+from .saved_query import SavedQuery
