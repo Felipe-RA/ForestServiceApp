772c33e675d9732d71ec260f44f9979b163b802a
felipe-ra <felipeangel50@gmail.com>
2023-11-20 23:19:31 -0500

    added documentation for schemas

--- backend/app/schemas/saved_query.py
+++ backend/app/schemas/saved_query.py
@@ -3,17 +3,43 @@ from typing import Optional
 from datetime import datetime
 
 class SavedQueryBase(BaseModel):
+    """
+    Base model for saved queries, defining common attributes.
+    Used as a base class for more specific saved query models.
+    Attributes:
+        name (str): The name assigned to the saved query.
+        query_text (str): The SQL text of the saved query.
+        username (str): The username of the user who created the query.
+    """
     name: str
     query_text: str
     username: str
 
 class SavedQueryCreate(SavedQueryBase):
+    """
+    Schema for creating a new saved query. Inherits from SavedQueryBase.
+    Adds an optional comment field.
+    Attributes:
+        comment (Optional[str]): An optional comment about the saved query.
+    """
     comment: Optional[str] = None
 
 class SavedQuery(SavedQueryBase):
+    """
+    Schema for representing saved queries, extending the base with additional attributes.
+    Attributes:
+        id (int): The unique identifier of the saved query.
+        created_at (Optional[datetime]): The timestamp when the query was saved.
+        comment (Optional[str]): An optional comment about the saved query.
+    """
     id: int
     created_at: Optional[datetime] = None
     comment: Optional[str] = None
 
     class Config:
+        """
+        Pydantic configuration class to enable ORM mode.
+        This allows the model to be compatible with SQLAlchemy models,
+        enabling direct usage of database models in FastAPI responses.
+        """
         orm_mode = True
--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -3,13 +3,33 @@ from typing import Optional
 from datetime import datetime
 
 class UserBase(BaseModel):
+    """
+    Base model for user, defining the core attribute.
+    Used as a base class for more specific user models.
+    Attributes:
+        username (str): The unique username of the user.
+    """
     username: str
 
 class UserCreate(UserBase):
+    """
+    Schema for creating a new user. Currently, this is identical to UserBase,
+    but it can be expanded with additional attributes in the future.
+    """
     pass
 
 class User(UserBase):
+    """
+    Schema for representing a user, extending the base with additional attributes.
+    Attributes:
+        registered_at (Optional[datetime]): The timestamp when the user was registered.
+    """
     registered_at: Optional[datetime] = None
 
     class Config:
+        """
+        Pydantic configuration class to enable ORM mode.
+        Allows the model to be compatible with SQLAlchemy models,
+        enabling the direct usage of database models in FastAPI responses.
+        """
         orm_mode = True
