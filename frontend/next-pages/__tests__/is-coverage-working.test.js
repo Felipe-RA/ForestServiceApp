b701e36c43b7c1580a049b0dc024fee6d6cdf9a4
felipe-ra <felipeangel50@gmail.com>
2023-11-21 11:15:35 -0500

    added health check test

---
+++ frontend/next-pages/__tests__/is-coverage-working.test.js
@@ -0,0 +1,7 @@
+// frontend/next-pages/__tests__/is-coverage-working.test.js
+
+
+// this test is meant to always pass, it will act as a health check for the automatic code testing/coverage
+test('basic test', () => {
+    expect(true).toBe(true);
+  });
